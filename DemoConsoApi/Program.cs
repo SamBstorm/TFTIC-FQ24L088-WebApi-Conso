2062f23530317b67c1f0ade6875d5826618ad956
Samuel Legrain <samuel.legrain@bstorm.be>
2025-01-31 11:37:16 +0100

    Add Demo Post

--- DemoConsoApi/Program.cs
+++ DemoConsoApi/Program.cs
@@ -1,6 +1,8 @@
 ﻿using DemoConsoApi.Models;
 using System.Net;
+using System.Text;
 using System.Text.Json;
+using System.Text.Unicode;
 
 namespace DemoConsoApi
 {
@@ -36,10 +38,49 @@ namespace DemoConsoApi
                             break;
                     }
                 }
-                catch(Newtonsoft.Json.JsonException jsonEx)
+                catch (Newtonsoft.Json.JsonException jsonEx)
                 {
                     Console.WriteLine($"L'information n'est pas du type attendu.");
                 }
+            }
+
+            Console.WriteLine("Voulez-vous ajouter une tâche ?");
+            Console.WriteLine("Quelle est le nom de la tâche ?");
+            string titre = Console.ReadLine();
+            var nouvelleTache = new TachePost() { Titre = titre }; // new {Titre = titre};
+            /*string json = JsonSerializer.Serialize(nouvelleTache);
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(API_URI);
+                HttpResponseMessage response = client.PostAsync("/api/Tache", content).Result;
+                response.EnsureSuccessStatusCode();
+                Console.WriteLine($"La tâche '{titre}' a bien été sauvée!");
+            }*/
+            try
+            {
+                AddTache(API_URI, "/api/Tache", nouvelleTache);
+                Console.WriteLine($"La tâche '{titre}' a bien été sauvée!");
+            }
+            catch (AggregateException ex)
+            {
+                try
+                {
+                    throw ex.InnerException;
+                }
+                catch (HttpRequestException httpEx)
+                {
+                    switch (httpEx.StatusCode)
+                    {
+                        case HttpStatusCode.NotFound:
+                            Console.WriteLine($"Route incorrecte vérifier votre URI, la route ou si le server API est allumé");
+                            break;
+                        case HttpStatusCode.Unauthorized:
+                            Console.WriteLine($"Vous n'avez pas le droit de publier une nouvelle tâche.");
+                            break;
+                    }
+                }
+
             }
         }
 
@@ -65,6 +106,18 @@ namespace DemoConsoApi
                     }
                 }*/
                 return await response.Content.ReadAsAsync<IEnumerable<Tache>>();
+            }
+        }
+
+        static async Task AddTache(string api_uri, string route, TachePost nouvelleTache)
+        {
+            string json = JsonSerializer.Serialize(nouvelleTache);
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(api_uri);
+                HttpResponseMessage response = await client.PostAsync(route, content);
+                response.EnsureSuccessStatusCode();
             }
         }
     }
