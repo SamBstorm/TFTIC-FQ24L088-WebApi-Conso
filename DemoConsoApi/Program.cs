d5af570f4b77eae3682af0f326247b00570b5b5a
Samuel Legrain <samuel.legrain@bstorm.be>
2025-01-31 10:52:22 +0100

    Ajoutez des fichiers projet.

---
+++ DemoConsoApi/Program.cs
@@ -0,0 +1,71 @@
+﻿using DemoConsoApi.Models;
+using System.Net;
+using System.Text.Json;
+
+namespace DemoConsoApi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string API_URI = "https://localhost:7217";
+
+            try
+            {
+                IEnumerable<Tache> taches = GetAllTache(API_URI, "/api/Tache").Result;
+                foreach (Tache tache in taches)
+                {
+                    Console.WriteLine($"{tache.Id} : {tache.Titre}");
+                }
+            }
+            catch (AggregateException ex)
+            {
+                try
+                {
+                    throw ex.InnerException;
+                }
+                catch (HttpRequestException httpEx)
+                {
+                    switch (httpEx.StatusCode)
+                    {
+                        case HttpStatusCode.NotFound:
+                            Console.WriteLine($"Route incorrecte vérifier votre URI, la route ou si le server API est allumé");
+                            break;
+                        case HttpStatusCode.Unauthorized:
+                            Console.WriteLine($"Vous n'avez pas l'autorisation, vérifier votre authentification.");
+                            break;
+                    }
+                }
+                catch(Newtonsoft.Json.JsonException jsonEx)
+                {
+                    Console.WriteLine($"L'information n'est pas du type attendu.");
+                }
+            }
+        }
+
+        static async Task<IEnumerable<Tache>> GetAllTache(string api_uri, string route)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(api_uri);
+                HttpResponseMessage response = await client.GetAsync(route);
+                response.EnsureSuccessStatusCode();
+                /*  Les bonnes pratiques imposent d'utiliser la méthode EnsureSuccessStatusCode car celle-ci retourne une Exception en cas d'échec
+                 * if (response.IsSuccessStatusCode)
+                {
+                    //string content = response.Content.ReadAsStringAsync().Result;
+                    //Console.WriteLine(content);
+                    //IEnumerable<Tache>? taches = JsonSerializer.Deserialize<IEnumerable<Tache>>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+
+                    IEnumerable<Tache> taches = response.Content.ReadAsAsync<IEnumerable<Tache>>().Result;
+                    foreach (Tache tache in taches)
+                    {
+                        Console.WriteLine($"{tache.Id} : {tache.Titre}");
+                    }
+                }*/
+                return await response.Content.ReadAsAsync<IEnumerable<Tache>>();
+            }
+        }
+    }
+}
